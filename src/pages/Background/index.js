642ade065539777f6182a0b5aab06e96143482ce
dali-benothmen <mohamedalibenothmen1@gmail.com>
2024-11-18 14:11:04 +0100

    Add notification for reaching maximum saved items limit

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,1 +1,10 @@
-console.log('Hello from Background...');
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'showNotification') {
+    chrome.notifications.create('reachedLimitNotification', {
+      type: 'basic',
+      iconUrl: 'icons/icon48.png',
+      title: 'CopySaver',
+      message: request.message,
+    });
+  }
+});
