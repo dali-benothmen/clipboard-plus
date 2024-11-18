907c1f7d83a435443a310e3076a7ed0a46539fe7
dali-benothmen <mohamedalibenothmen1@gmail.com>
2024-11-18 11:22:28 +0100

    Initialize project setup with React, Typescript, Webpack, and Babel.

---
+++ src/pages/Popup/Popup.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Popup.css';
+
+const Popup = () => {
+  return <div className="App">This is Popup</div>;
+};
+
+export default Popup;
---
+++ utils/webserver.js
@@ -0,0 +1,55 @@
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV = 'development';
+process.env.ASSET_PATH = '/';
+
+const WebpackDevServer = require('webpack-dev-server');
+const webpack = require('webpack');
+const config = require('../webpack.config');
+const env = require('./env');
+const path = require('path');
+
+const options = config.chromeExtensionBoilerplate || {};
+const excludeEntriesToHotReload = options.notHotReload || [];
+
+for (const entryName in config.entry) {
+  if (excludeEntriesToHotReload.indexOf(entryName) === -1) {
+    config.entry[entryName] = [
+      'webpack/hot/dev-server',
+      `webpack-dev-server/client?hot=true&hostname=localhost&port=${env.PORT}`,
+    ].concat(config.entry[entryName]);
+  }
+}
+
+delete config.chromeExtensionBoilerplate;
+
+const compiler = webpack(config);
+
+const server = new WebpackDevServer(
+  {
+    https: false,
+    hot: true,
+    liveReload: false,
+    client: {
+      webSocketTransport: 'sockjs',
+    },
+    webSocketServer: 'sockjs',
+    host: 'localhost',
+    port: env.PORT,
+    static: {
+      directory: path.join(__dirname, '../build'),
+    },
+    devMiddleware: {
+      publicPath: `http://localhost:${env.PORT}/`,
+      writeToDisk: true,
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    allowedHosts: 'all',
+  },
+  compiler
+);
+
+(async () => {
+  await server.start();
+})();
